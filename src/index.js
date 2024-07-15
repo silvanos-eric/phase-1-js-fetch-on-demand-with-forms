e8f90f0b40b41ad80b2ff951ac35ae492e2933a2
Silvanos Eric <ericoderito@gmail.com>
2024-07-15 08:05:46 +0300

    feat: enhance search functionality and improve HTML structure
    - Update HTML structure in index.html to improve readability and consistency in closing tags.
    - Add JavaScript event listener for form submission to handle movie searches by ID. Implement fetch
      request to retrieve and display movie details from a local server based on the movie ID provided.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+  const inputFormEl = document.querySelector("form");
+  inputFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+    const searchByIDInputEl = event.target.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${searchByIDInputEl.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const titleEl = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        titleEl.textContent = data.title;
+        summary.textContent = data.summary;
+
+        searchByIDInputEl.value = "";
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
